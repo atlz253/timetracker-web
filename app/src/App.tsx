b8ff2aee53945858dd2aa4323396f1b5e87c7890
Fedor <f.alexeeff2010@yandex.ru>
2021-01-30 12:21:24 +0300

    App реализован с помощью класса

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,20 +2,35 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { AuthPage } from "./pages/AuthPage";
 
-const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <div id="wrapper">
-        <div id="container">
-          <Switch>
-            <Route path="/">
-              <AuthPage />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+
+interface IAppState
+{
+  isLoggedIn: boolean,
+  authToken: string
 }
 
-export default App;
+
+export class App extends React.Component<{}, IAppState>
+{
+  state: IAppState = {
+    isLoggedIn: false,
+    authToken: ""
+  }
+
+  render()
+  {
+    return (
+      <BrowserRouter>
+        <div id="wrapper">
+          <div id="container">
+            <Switch>
+              <Route path="/">
+                <AuthPage />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
