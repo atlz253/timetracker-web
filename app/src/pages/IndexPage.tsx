fd974c9e824c3c79649ab2ec6962c0bb827217d1
Fedor <f.alexeeff2010@yandex.ru>
2021-01-31 17:34:04 +0300

    Добавил условный рендер

--- app/src/pages/IndexPage.tsx
+++ app/src/pages/IndexPage.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 
 
-export class IndexPage extends React.Component<{}, {}>
+interface IIndexPageProps
+{
+    loginState: boolean
+}
+
+
+export class IndexPage extends React.Component<IIndexPageProps, {}>
 {
     render()
     {
-        return(
-            <h1>Hello world!</h1>
-        )
+        if (this.props.loginState)
+            return(
+                <div id="container">
+                    <h1>Вы в деле!</h1>
+                    <p>
+                        Время добавлять новые активности ;)
+                    </p>
+                </div>
+            );
+        else
+            return(
+                <div id="container">
+                    <h1>Добро пожаловать!</h1>
+                    <p>
+                        Чтобы воспользоваться мощью нашей технологии вы должны быть авторизованы
+                    </p>
+                </div>
+            );
     }
 }
