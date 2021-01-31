ba569c6be164ff9f5a7c164384764139ed9490f3
Fedor <f.alexeeff2010@yandex.ru>
2021-01-31 15:36:38 +0300

    Провел оптимизацию компиляции sass

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,12 +1,21 @@
-const dist = "*";
+const dist = "/home/fedor/Projects/timetracker-web/app/public/";
 const dir = {
-    dist: 
+    main_css:
     {
-        main_css: `${dist}/css/`
+        src: "src/sass/main/main.sass",
+        dest: `${dist}/css/`,
+        dependencies: [
+            "src/sass/variables.sass",
+            "src/sass/components/Header/Header.sass"
+        ]
     },
-    src: 
+    loginPage_css:
     {
-        main_sass: "src/sass/main/main.sass"
+        src: "src/sass/pages/LoginPage/LoginPage.sass",
+        dest: `${dist}/css/pages/`,
+        dependencies: [
+            "src/sass/variables.sass",
+        ]
     }
 };
 
@@ -16,14 +25,23 @@ const gulp = require('gulp'),
       { series, parallel, watch } = require('gulp');
 
 
-function sass_compile() {
-    return gulp.src(dir.src.main_sass)
-        .pipe(sass())
-        .pipe(gulp.dest(dir.dist.main_css))
+function sass_compile(src, dest) {
+    return gulp.src(src)
+    .pipe(sass())
+    .pipe(gulp.dest(dest));
 };
 
 
 exports.default = function() {
-    watch([dir.src.main_sass, "src/sass/components/AuthPage/LoginForm.sass"], sass_compile)
-}
+    watch([dir.main_css.src, ...dir.main_css.dependencies], main_css = (cb) => { 
+        sass_compile(dir.main_css.src, dir.main_css.dest);
+        cb();
+    });
+    watch([dir.loginPage_css.src, ...dir.loginPage_css.dependencies], loginPage_css = (cb) => {
+        sass_compile(dir.loginPage_css.src, dir.loginPage_css.dest);
+        cb();
+    });
+};
+
+
 exports.build = series(sass_compile);
